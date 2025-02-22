773d071f1e2cde624b33a99aa822abd2269d7fa4
Akash4693 <viperholic46@gmail.com>
2025-02-22 09:22:31 +0530

    update

--- server/db/connectDB.ts
+++ server/db/connectDB.ts
@@ -1,13 +1,13 @@
-// mongopassword=jgUYh0afhTqzd9dC
-// asurendrakumarpatel
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI!);
-        console.log('mongoDB connected.');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://viperholic:akash12345@cluster0.m0eniok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/rhezo"!
+    );
+    console.log("mongoDB connected.");
+  } catch (error) {
+    console.log(error);
+  }
+};
 export default connectDB;
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import connectDB from "./db/connectDB";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -15,19 +16,26 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+console.log("Port: ", PORT);
+console.log("PORT: ", process.env.PORT);
 
 const DIRNAME = path.resolve();
+console.log(DIRNAME);
 
 // default middleware for any mern project
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://food-app-yt.onrender.com",
-    credentials: true
-}
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 // api
 app.use("/api/v1/user", userRoute);
@@ -35,12 +43,12 @@ app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
 app.use("/api/v1/order", orderRoute);
 
-app.use(express.static(path.join(DIRNAME,"/client/dist")));
-app.use("*",(_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+app.use(express.static(path.join(DIRNAME, "/client/dist")));
+app.use("*", (_, res) => {
+  res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server listen at port ${PORT}`);
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
 });
